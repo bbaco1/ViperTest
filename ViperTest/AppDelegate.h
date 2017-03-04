ccf92c6f359a345aacb16285a8ccb9853a7b6ba9
Milorad Orzes <miloradorzes@milorads-mac.lan>
2017-03-04 18:31:03 +0100

    Initial Commit

---
+++ ViperTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViperTest
+//
+//  Created by Milorad Orzes on 04/03/2017.
+//  Copyright © 2017 Milorad Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViperTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViperTest
+//
+//  Created by Milorad Orzes on 04/03/2017.
+//  Copyright © 2017 Milorad Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
